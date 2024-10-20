58c5fbb34a705e66f1835f9969807af947e82cbe
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-21 03:06:16 +0600

    type

---
+++ introduction/type.js
@@ -0,0 +1,20 @@
+// **********Number***************
+var price = 1000;
+console.log( typeof price);
+console.log(price);
+
+
+
+// *********String*************
+
+var name = "Sabbir Hasan"
+console.log(typeof name);
+console.log(name);
+
+
+
+// ************Boolean*************
+
+var ispoor = true;
+console.log(typeof ispoor);
+console.log(ispoor);
