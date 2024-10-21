51cdcfaa344cc4e05d1df0a3ee81a175580dc472
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-22 04:13:22 +0600

    number.js

---
+++ introduction/number.js
@@ -0,0 +1,11 @@
+var applePrice = 100.50;
+// var orrangePrice = 90.65;
+var orrangePrice = parseFloat('90.65');
+// console.log(applePrice + orrangePrice);
+
+
+var num1 = 50;
+var num2 = 45;
+var dvide = num1 / num2;
+console.log( typeof parseFloat(dvide.toFixed(3)));
+console.log(parseFloat(dvide.toFixed(3)));
