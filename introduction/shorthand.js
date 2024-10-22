91c88d8596e55ca8453b583547815737040075e1
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-23 01:25:47 +0600

    shorthand

---
+++ introduction/shorthand.js
@@ -0,0 +1,25 @@
+var orrangePrice = 30;
+// orrangePrice = orrangePrice * 3;
+orrangePrice *=3;
+console.log(orrangePrice);
+
+var applePrice = 50;
+// applePrice = applePrice + 30;
+applePrice +=30;
+console.log(applePrice);
+
+
+// var ticketPrice = 800;
+// ticketPrice = ticketPrice - 50;
+// console.log(ticketPrice);
+
+var ticketPrice = 650;
+ ticketPrice +=100;
+ console.log(ticketPrice);
+
+
+//  ******************************MixedOp*********************************
+ var name1 = 'Sabbir';
+ var name2 = 'Hasan';
+ var fullName = name1 + " " + name2;
+ console.log(fullName);
