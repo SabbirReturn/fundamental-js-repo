ddfb7d1d35a28143fdac83d00f166ad41a5fe782
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-23 21:35:10 +0600

    maltiple

---
+++ condition/maltiple.js
@@ -0,0 +1,30 @@
+// var salary = 30001;
+// var hasJob = true;
+// var isBcs = true;
+// if((salary>30005 && hasJob == true)|| isBcs == true){
+//     console.log('Bolo baba kobul');
+// }
+// else{
+//     console.log('Vago')
+// }
+
+// var height = 72;
+// var weight = 78;
+// var hasCar = true;
+// var isBcs = false;
+// if((isBcs==true) || hasCar==true && weight>85 && height>70){
+//     console.log('I like her')
+// }
+// else{
+//     console.log('Please give me a another option')
+// }
+
+var ticketPrice = 650;
+isEid = false;
+if(isEid == true){
+    newTicketPrice = ticketPrice + 100;
+    console.log(newTicketPrice)
+}
+else{
+    console.log(ticketPrice);
+}
