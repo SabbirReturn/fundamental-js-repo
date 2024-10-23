3819856c8438dad47c836f7fc3d463f200596acb
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-23 20:49:56 +0600

    If else

---
+++ condition/if.js
@@ -0,0 +1,25 @@
+// .......................if else structure............................................
+// if(condition){
+//     execute the code when condition is true
+// }
+// else{
+//     execute the code when condition is false
+// }
+
+
+
+// var birianyPrice = 300;
+// if(birianyPrice < 400){
+//     console.log('Give me one plate Biriany')
+// }
+// else{
+//     console.log('Give me one plate vat')
+// }
+
+var ticketPrice = 750;
+if(ticketPrice < 700){
+    console.log("Give me one seat")
+}
+else{
+    console.log("I will not go by Bus")
+}
