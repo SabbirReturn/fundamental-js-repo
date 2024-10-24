c5b4da13de8381a98c54c5d50306b42b948274d9
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-25 01:37:47 +0600

    nasted_if

---
+++ condition/nasted_if.js
@@ -0,0 +1,22 @@
+const ticketPrice = 650;
+const isStudent = false;
+const isGeneralPassenger = true;
+const isEid = false;
+if(isStudent == true && isGeneralPassenger== true && isEid == true){
+
+    // 100 tk add
+    const addmoney = ticketPrice + 100;
+    console.log(addmoney);
+    
+}
+else{
+    if(isStudent == true && isGeneralPassenger== true){
+        // 50% discount.............
+        const discountAmount = ticketPrice * 50/100;
+        const payAmount = ticketPrice - discountAmount;
+        console.log(payAmount);
+    }
+    else{
+        console.log(ticketPrice);
+    }
+}
