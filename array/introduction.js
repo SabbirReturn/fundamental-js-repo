e2e784bf7b3d3b2d004f0bdd0ec482d16f4d49fe
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-26 02:53:50 +0600

    length

---
+++ array/introduction.js
@@ -0,0 +1,3 @@
+const friendName = ['Sabbir','Hasan','Robin','Misu'];
+const numbers = [1,2,3,4,5,6,7,8,9];
+const fruitsName = ['Mango','Apple','Banana','Orange']
---
+++ array/length.js
@@ -0,0 +1,6 @@
+const numbers = [1,2,3,4,5,6,7,8];
+// console.log(numbers);
+// console.log(numbers.length);
+
+const friendName = ['Sabbir','Hasan','Robin','Misu'];
+console.log(friendName.length);
