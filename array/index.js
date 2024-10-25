9e40b6e36ecd31e9a2ccfa72cc6aec86bf528e79
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-26 02:57:04 +0600

    index

---
+++ array/index.js
@@ -0,0 +1,6 @@
+const friendName = ['Sabbir','Hasan','Robin','Misu'];
+// console.log(friendName[2]);
+friendName[2]= 'Nil';
+console.log(friendName);
+const name = friendName[3];
+console.log(name)
