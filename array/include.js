f266593e2f8139d77c085b65c4d530b4a8601051
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-27 02:30:35 +0600

    include

---
+++ array/include.js
@@ -0,0 +1,8 @@
+const friendName = ['Sabbi','Hasan','Misu','Robin'];
+console.log(friendName.includes('Hasan'));
+if(friendName.includes('Sabbir')){
+    console.log('Bus charo');
+}
+else{
+    console.log('Wait a minute')
+}
