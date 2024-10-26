6b67a28e381cc235f7fb0546161a21c058ce3950
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-27 02:35:18 +0600

    isArry

---
+++ array/isArray.js
@@ -0,0 +1,6 @@
+const friendName = ['Sabbir','Hasan','Misu','Robin'];
+const numbers = [];
+const name = '[]'
+console.log(Array.isArray(friendName));
+console.log(Array.isArray(numbers));
+console.log(Array.isArray(name));
