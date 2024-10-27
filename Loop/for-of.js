ac8a41a2a01bad92332709f455192e1dcf7033ed
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-27 21:17:26 +0600

    for-of

---
+++ Loop/for-of.js
@@ -0,0 +1,11 @@
+// const numbers = [1,2,3,4,5,6,7,8];
+// console.log(numbers);
+// for(const num of numbers){
+//     console.log(num);
+// }
+const fruitName = ['Mango','Apple','Orange','Jambura'];
+console.log(fruitName);
+for(const fruit of fruitName){
+    console.log(fruit);
+    console.log('I want to eat');
+}
