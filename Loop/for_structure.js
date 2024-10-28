b2b669a3e27976b0c96483c1b9cfb16f4f83c8c2
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-29 02:26:03 +0600

    for_structure

---
+++ Loop/for_structure.js
@@ -0,0 +1,17 @@
+// for (declianre variable ; condition ; change vallue){
+// }
+
+
+// let sum = 0;
+// for(let i =0 ; i<=10; i++ ){
+//     console.log(i);
+//     sum = sum + i;
+//     console.log('Total',sum);
+// }
+
+for(let i= 0; i<=10 ; i++){
+    // console.log(i)
+    if(i % 2 ===1){
+        console.log('odd',i);
+    }
+}
