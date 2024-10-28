f15a09a5cddbd0d24566068aa932ce6dd011433c
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-29 01:39:28 +0600

    range

---
+++ Loop/while_loop.js
@@ -0,0 +1,13 @@
+// let num = 0;
+// while(num < 5){
+//     console.log('LOOping',num);
+//     num+=1;
+// }
+
+let num= 0;
+while(num<100){
+    console.log(num);
+    const oddNumber = num /2
+    console.log(oddNumber)
+    num+=1
+}
