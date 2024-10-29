adbc6e1c1917c6172fe80be38a6c85739df47219
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-30 01:57:04 +0600

    break

---
+++ Loop/break.js
@@ -0,0 +1,18 @@
+// for( let i = 0; i<100 ; i++){
+//     console.log(i);
+//     if(i>=10){
+//         break
+//     }
+// }
+// console.log('LIfe after Breakup')
+
+// ....Using while............. break= end the loop
+let i= 100;
+let sum = 0;
+while(i>=10){
+    console.log(i);
+    if(i<=50){
+        break
+    }
+    i--
+}
