af829997d3e365a900a30f1f1b9c679a32b61b58
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-29 20:57:05 +0600

    decremental

---
+++ Loop/decremental-for.js
@@ -0,0 +1,9 @@
+// for( let i = 10; i>=0 ; i--){
+//     console.log(i);
+// }
+let sum = 0;
+for ( let i= 20; i>=10; i--){
+    console.log(i);
+    sum = sum + i;
+    console.log('Total',sum);
+}
