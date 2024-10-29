85fa7d5d90407ef084424bd47257fa2f429e107e
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-30 02:12:07 +0600

    continue

---
+++ Loop/continue.js
@@ -0,0 +1,18 @@
+// for( let i= 0 ; i<20; i++){
+//     
+//     if( i % 2===0){
+//         continue
+//     }
+// console.log(i);
+// }
+
+
+let i = 0;
+while(i < 20){
+    i++
+    if( i % 2 ===0){
+        continue
+    }
+    console.log(i)
+    
+}
