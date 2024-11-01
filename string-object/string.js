2a421c63551e6f343e360651d24be45b036e4dab
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-02 00:41:46 +0600

    string

---
+++ string-object/string.js
@@ -0,0 +1,10 @@
+const Division = 'Dhaka';
+const thana = 'Uttara';
+const location = new String('Demra')
+console.log(thana);
+console.log(typeof thana);
+// console.log(location);
+console.log(Division.length);
+console.log(Division[0]);
+// Division[0]= ' F'      String is emmutable
+console.log(Division);
