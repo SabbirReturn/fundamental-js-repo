8e90356680a7228454f0a9ac303220a03d09a09e
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-02 15:31:53 +0600

    while

---
+++ recap-condition-array-loop/while.js
@@ -0,0 +1,26 @@
+// let i = 1451;
+// let sum = 0;
+// while(i <=2450){
+//     // console.log(i);
+//     if(i % 2 ===0){
+//         console.log('Even number',i);
+//         sum = sum + i;
+//         console.log('total',sum);
+//     }
+//     i++
+// }
+
+
+// reverse number
+
+let i =50;
+while(i >=10){
+    // console.log(i)
+    i--
+    if(i % 5 ===0){
+        continue;
+        
+        
+    }
+    console.log(i);
+}
