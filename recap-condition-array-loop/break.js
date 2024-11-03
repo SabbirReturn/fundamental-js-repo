2c3d8642c59bd0e20b98a8316bc4485bd76e39f2
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-03 20:58:58 +0600

    reverse

--- recap-condition-array-loop/break.js
+++ recap-condition-array-loop/break.js
@@ -1,7 +1,19 @@
-for( let i= 0; i<50; i++){
-    // console.log(i);
-    if(i>40){
+// for( let i= 0; i<50; i++){
+//     // console.log(i);
+//     if(i>40){
+//         break
+//     }
+//     console.log(i);
+// }
+
+
+
+// using reverse include break..........
+let i= 50;
+while(i> 10){
+    console.log(i);
+    i--
+    if(i<20){
         break
     }
-    console.log(i);
 }
