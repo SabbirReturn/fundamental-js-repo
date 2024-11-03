2e7f617bae1e7df139deb758b6e46744d7fa1eb2
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-04 03:02:10 +0600

    slice,split,join

---
+++ string-object/slice.js
@@ -0,0 +1,12 @@
+let address = 'Uttara'
+// console.log(address.slice(0,3));
+
+let sentence = 'I am from Dhaka';
+// console.log(sentence.split(" "))
+
+let friends = ['Sabbir','Hasan','Robin']
+let friend = friends.join("+")
+// console.log(friend)
+
+let word = ["hello", "world"];
+console.log(word.join("+"))
