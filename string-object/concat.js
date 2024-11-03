95a44ebf87be3d37ee4ce3412f2fecf6f3042b73
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-04 03:09:29 +0600

    concat

---
+++ string-object/concat.js
@@ -0,0 +1,16 @@
+let word1 = 'Hello';
+let word2 = 'World';
+let fullWord = word1.concat(" ").concat(word2)
+// console.log(fullWord);
+
+
+let firstName = 'Sabbir';
+let lastName = 'Hasan';
+let fullname = firstName.concat(" ").concat(lastName);
+let first = firstName.slice(0,4);
+console.log(first);
+console.log(fullname);
+console.log(typeof fullname);
+let fullnameSplite = fullname.split(',')
+console.log(fullnameSplite)
+
