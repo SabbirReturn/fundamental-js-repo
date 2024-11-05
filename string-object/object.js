876480e8a90e42a2a8723d1e531785291b2ef491
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-05 21:05:02 +0600

    object-introduction

---
+++ string-object/object.js
@@ -0,0 +1,12 @@
+const man = {
+    name: 'Sabbir',
+    age : 24,
+    location :'Uttara',
+    profession :'Web Developer',
+    isClean : true,
+    guardianName :{
+        mother : 'Mst Reshma Begum',
+        father :'Md Emdadul Haque',
+    }
+}
+console.log(man);
