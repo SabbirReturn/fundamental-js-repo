cf2a0c039cce4f36baaa1690639edc69e94944d2
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-07 03:25:18 +0600

    value

---
+++ string-object/value.js
@@ -0,0 +1,15 @@
+let person = {
+    name : 'sabbir Hasan',
+    age : 25,
+    profession : 'web-developer',
+    'Fav place': ['Coxs Bazar','Kushtia'],
+}
+// console.log(person);
+
+// const prop = person['profession'];
+// console.log(prop);
+person.age = 26;
+// console.log(person['age']);
+
+const keyName = 'profession';
+console.log(person[keyName])
