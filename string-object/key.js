5cec5eabf8a4fec488c3107890b5451d147820bb
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-08 18:09:20 +0600

    key

---
+++ string-object/key.js
@@ -0,0 +1,13 @@
+const pc ={
+    brand : 'Megabite',
+    monitor : ' MSI ',
+    cpu :'Tharmal Tech',
+    madarboard : 'Megabite',
+    ssd : '256GB',
+}
+// console.log(pc);
+const keys = Object.keys(pc);
+console.log(keys);
+for ( const key of keys){
+    console.log(key);
+}
