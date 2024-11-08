42b87ad4cac8405bc2b02506a070f207474d1541
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-08 18:24:58 +0600

    values

---
+++ string-object/values.js
@@ -0,0 +1,13 @@
+const pc ={
+    brand : 'Megabite',
+    monitor : ' MSI ',
+    cpu :'Tharmal Tech',
+    madarboard : 'Megabite',
+    ssd : '256GB',
+}
+console.log(pc);
+const values = Object.values(pc);
+console.log(values);
+for(const value of values){
+    console.log(value)
+}
