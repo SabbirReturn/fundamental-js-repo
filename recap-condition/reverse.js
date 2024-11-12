86de90886cd89f771a303f9a73aaa4c637422197
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-12 23:17:16 +0600

    reverse

---
+++ recap-condition/reverse.js
@@ -0,0 +1,11 @@
+let sentence = ' I am Sabbir Hasan';
+let i = 0;
+let reverse = '';
+while ( i < sentence.length){
+    // console.log(i);
+    
+    console.log(sentence[i]);
+    reverse = sentence[i] + reverse;
+    console.log(reverse);
+    i++
+}
