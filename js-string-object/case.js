a0ac6795350f0526f552cf43aec15550c6279096
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-13 04:11:12 +0600

    case

---
+++ js-string-object/case.js
@@ -0,0 +1,11 @@
+const schoolName = 'Bheramara Pilot High School';
+// console.log(schoolName.toUpperCase());
+
+let subject = 'Chemistry';
+let examName ='chemistry';
+if(subject.toUpperCase() ===examName.toUpperCase()){
+    console.log('Get A+')
+}
+else{
+    console.log('Get F+')
+}
