16a5b0b09851ad12332d99980699b9a7493dd455
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-14 03:25:13 +0600

    slice,split,join,concat,includes

---
+++ js-string-object/slice.js
@@ -0,0 +1,22 @@
+let address = 'Uttara sector ten';
+// console.log(address.slice(0,4));
+
+let from = 'I am from Bangladesh';
+// console.log(from.split(' '));
+
+let friendstr = 'Sabbir, Hasan, Robin';
+// console.log(friendstr.split(','));
+
+let realFriend = [ 'Sabbir',' Hasan',' Robin'];
+// console.log(realFriend.join('+'));
+
+if(realFriend.includes('Sabbir')){
+    // console.log('go to cox bazar')
+}
+
+// concat
+
+let firstName = 'Sabbir';
+let lastName = 'Hasan';
+let fullname = firstName.concat(' ').concat(lastName);
+console.log(fullname);
