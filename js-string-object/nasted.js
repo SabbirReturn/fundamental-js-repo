54965883bef2362bc8e4af9c4cf02df57353cafd
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-18 04:21:26 +0600

    nasted

---
+++ js-string-object/nasted.js
@@ -0,0 +1,16 @@
+let school={
+    name : ' Bheramara Pilot High School',
+    Class:['6','7','8','9','10'],
+    events :['21 february','26 march','16 december'],
+    unique:{
+        color : 'White',
+        result:{
+            gpa: 5,
+            merit : ' Top Top'
+        }
+    }
+}
+school.events[1] = 'bijoy Dibos'
+school.unique.result.merit = 'top top top most'
+
+console.log(school);
