590cec660cbb9d6ef0466d839aaa492156acbc06
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-18 04:12:40 +0600

    key-value

---
+++ js-string-object/key-value.js
@@ -0,0 +1,14 @@
+let computer ={
+    brand: 'Lenevo',
+    price : 40000,
+    hdd : '500gb',
+    cpu:{
+        matherbord : 'Gygabite',
+        processor : 'Ryzen'
+    }
+}
+// console.log(computer);
+let key = Object.keys(computer);
+let value = Object.values(computer);
+console.log(key);
+console.log(value);
