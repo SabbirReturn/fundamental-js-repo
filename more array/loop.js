35f4f4479ba267bfa0096fbfae51a43bcb463240
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-20 02:05:30 +0600

    loop

---
+++ more array/loop.js
@@ -0,0 +1,19 @@
+let text = 'I am a Sabbir Hasan';
+for ( let word of text){
+    // console.log(word);
+}
+for( let i= 0; i<text.length; i++){
+    // console.log(i);
+    // console.log(text[i]);
+}
+
+let numbers = [12,13,14,15,16,17,18];
+let n = 0;
+while(n<numbers.length){
+    // console.log(n);
+    console.log(numbers[n]);
+    n++
+}
+for(let number of numbers){
+    console.log(number);
+}
