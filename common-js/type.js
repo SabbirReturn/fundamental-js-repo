2c88fe11b3768a6257a941e04c04b4a6ce565b85
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-05-17 23:48:30 +0600

    again

--- common-js/type.js
+++ common-js/type.js
@@ -11,6 +11,19 @@
 
 // let array = Array.isArray(numbers);
 // console.log(array);
+// let numbers = [1,2,3,4,5,6,7,8,];
+// let age = 28;
+// let name = 'Sabbbir';
+// let products={
+//      id:1,
+//      name:'Sabbir'
+// }
+// let isStudent = true;
+
+// console.log(typeof numbers,typeof age,typeof name,typeof products,typeof isStudent);
+
+// let array = Array.isArray(numbers);
+// console.log(array);
 let numbers = [1,2,3,4,5,6,7,8,];
 let age = 28;
 let name = 'Sabbbir';
